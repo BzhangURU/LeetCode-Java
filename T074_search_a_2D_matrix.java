b49c8fe5fb08a13f8b80db87180d54d1e43a0e34
Barton <bodong.zhang@yahoo.com>
2017-01-28 03:02:14 -0700

    Create T074_search_a_2D_matrix.java

---
+++ T074_search_a_2D_matrix.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null||matrix.length==0)
+        	return false;
+        if(matrix[0].length==0)
+        	return false;
+        int LINE=matrix.length;
+        int COLUMN=matrix[0].length;
+        int min=0;
+        int max=LINE*COLUMN-1;
+        
+        if(target<matrix[0][0])
+        	return false;
+        else if(target==matrix[0][0])
+        	return true;
+        if(target>matrix[LINE-1][COLUMN-1])
+        	return false;
+        else if(target==matrix[LINE-1][COLUMN-1])
+        	return true;
+        
+        int middle;
+        
+        while(true){
+        	middle=(max+min)/2;
+        	
+        	if(matrix[middle/COLUMN][middle%COLUMN]<target)
+        		min=middle;
+        	else if(matrix[middle/COLUMN][middle%COLUMN]>target)
+        		max=middle;
+        	else{
+        		return true;
+        	}
+        	if(min+1==max)
+        		return false;
+        }
+    }
+}
