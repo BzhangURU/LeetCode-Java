7be809c201533bab3f1bbd6d0d669f6b309de99f
Barton <bodong.zhang@yahoo.com>
2017-01-28 04:18:44 -0700

    Update T075_Sort_Colors.java

--- T075_Sort_Colors.java
+++ T075_Sort_Colors.java
@@ -12,7 +12,7 @@ A rather straight forward solution is a two-pass algorithm using counting sort.
 First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
 
 Could you come up with an one-pass algorithm using only constant space?*/
-//one pass
+//one pass using constant space
 public class Solution {
     public void sortColors(int[] nums) {
         if(nums==null||nums.length<=1)
@@ -36,7 +36,7 @@ public class Solution {
                 	left++;
         		while(right>=0&&nums[right]==2)
                 	right--;
-                if(left>point)
+                	if(left>point)
         			point=left;
         	}else if(nums[point]==2){
         		nums[point]=nums[right];
@@ -45,7 +45,7 @@ public class Solution {
                 	left++;
         		while(right>=0&&nums[right]==2)
                 	right--;
-                if(left>point)
+                	if(left>point)
         			point=left;
         	}else point++;
         	
