090eff68939439974f84c8496851198a5446b8d5
Barton <bodong.zhang@yahoo.com>
2017-01-31 16:50:40 -0700

    Create T113_Path_Sum_II.java

---
+++ T113_Path_Sum_II.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+		List<List<Integer>> LofL=new LinkedList<List<Integer>>();
+		List<Integer> List =new LinkedList<Integer>();
+		get_PathSum(root,sum,LofL,List);
+		return LofL;
+    }
+	
+	public void get_PathSum(TreeNode root, int sum,List<List<Integer>> LofL, List<Integer> List) {
+		
+        if(root==null)
+        	return;
+        if(root.left==null&&root.right==null)
+        	if(root.val==sum){
+        		List.add(root.val);
+        		List<Integer> List_output =new LinkedList<Integer>();
+        		for(int i=0;i<List.size();i++)
+        			List_output.add(List.get(i));
+        		List.remove(List.size()-1);
+        		LofL.add(List_output);
+        	}
+        	else return;
+        else if(root.left==null){
+        	
+        	List.add(root.val);
+        	get_PathSum(root.right,sum-root.val,LofL,List);
+        	List.remove(List.size()-1);
+        }
+        else if(root.right==null){
+        	
+        	List.add(root.val);
+        	get_PathSum(root.left,sum-root.val,LofL,List);
+        	List.remove(List.size()-1);
+        }
+        else{ 
+        	List.add(root.val);
+        	get_PathSum(root.left,sum-root.val,LofL,List);
+        	get_PathSum(root.right,sum-root.val,LofL,List);
+        	List.remove(List.size()-1);
+        }
+        
+    }
+}
