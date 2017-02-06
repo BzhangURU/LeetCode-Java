e5a6972981a0a1a79d5901e413f527bb53c0c115
Barton <bodong.zhang@yahoo.com>
2017-02-05 23:49:57 -0700

    Create T231_Power_of_Two.java

---
+++ T231_Power_of_Two.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0)
+        	return false;
+		int count=0;
+        
+        for(int i=0;i<32;i++){
+        	if(((n>>i)&1)==1)
+        		count++;
+        }
+        
+        return count==1;
+    }
+}
