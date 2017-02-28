47e97e66ca5efa06e0eb7573af80241a538a2083
Barton <bodong.zhang@yahoo.com>
2017-02-27 23:43:05 -0700

    Update T124_Binary_Tree_Maximum_Path_Sum.java

--- T124_Binary_Tree_Maximum_Path_Sum.java
+++ T124_Binary_Tree_Maximum_Path_Sum.java
@@ -11,47 +11,42 @@ public class Solution {
     public int maxPathSum(TreeNode root) {
 		if(root==null)
 			return 0;
-		HashMap<TreeNode,Integer> node_max=new HashMap<TreeNode,Integer>();
+		//max_start_from_this, can only go down
 		int[] max=new int[1];
 		max[0]=Integer.MIN_VALUE;
-		traverse_build_map(root,node_max,max);
+		traverse_build_map(root,max);
 		return max[0];
     }
-	public void traverse_build_map(TreeNode node,HashMap<TreeNode,Integer> node_max,int[] max){
+	
+	public int traverse_build_map(TreeNode node,int[] max){
 		
 		if(node.left==null&&node.right==null){
-			node_max.put(node, node.val);
 			if(node.val>max[0])
 				max[0]=node.val;
+			return node.val;
 		}else {
 			
 			if(node.left==null){
-				traverse_build_map(node.right,node_max,max);
-				int value=node.val+Math.max(0, node_max.get(node.right));
-				node_max.put(node, value);
+				int value=node.val+Math.max(0, traverse_build_map(node.right,max));
 				if(value>max[0])
 					max[0]=value;
-				
+				return value;
 			}else if(node.right==null){
-				traverse_build_map(node.left,node_max,max);
-				int value=node.val+Math.max(0, node_max.get(node.left));
-				node_max.put(node, value);
-				
+				int value=node.val+Math.max(0, traverse_build_map(node.left,max));
 				if(value>max[0])
 					max[0]=value;
+				return value;
 				
 			}else{
-				traverse_build_map(node.left,node_max,max);
-				traverse_build_map(node.right,node_max,max);
-				
-				
-				int value_l=Math.max(0, node_max.get(node.left));
-				int value_r=node_max.get(node.right);
-				int value_path=node.val+value_l+Math.max(0,value_r);
-				int value=node.val+Math.max(value_l,value_r);
-				node_max.put(node, value);
+				int value_l=traverse_build_map(node.left,max);
+				int value_r=traverse_build_map(node.right,max);
+				/*int value_path=node.val+Math.max(0, node_max.get(node.left))+
+						Math.max(0,node_max.get(node.right));*/
+				int value_path=node.val+Math.max(0,value_l)+Math.max(0,value_r);
+				int value=node.val+Math.max(0,Math.max(value_l,value_r));
 				if(value_path>max[0])
 					max[0]=value_path;
+				return value;
 			}
 			
 			
